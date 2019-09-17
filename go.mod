b09f52af4718668dd22c559c62bdcad8027969a1
Leopoldo Caballero <git@pitakill.net>
2019-09-17 18:41:36 -0500

    First version & Github Actions

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pitakill/ci-cd-example
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+var addr = ":8080"
+
+func main() {
+	http.HandleFunc("/", root)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(":-P version 1"))
+}
